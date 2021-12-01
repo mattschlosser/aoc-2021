c249beabf9e0b23595de5f0457a4980584b6e050
Matt Schlosser <hello@mattschlosser.me>
2021-12-01 05:24:25 +0000

    1

--- 1.js
+++ 1.js
@@ -1,10 +1,26 @@
 const fs = require('fs');
+const { exit } = require('process');
 let buffer = fs.readFileSync('1');
 let string = buffer.toString();
-let lines = string.split('\n')
-let i = null;
-for (let line of lines) {
-    let tokens = line.split(' ');
+let items = string.split('\n\n');
+let i = 0;
 
+function movAvg(items) {
+    let sum =  items.reduce((a,e) => a+ (+e),0);
+    return sum;
+}
+for (let item of items) {
+    let depths = string.split('\n')
+    for (let depth in depths) {
+        if (depth == 0 || depth == 1 || depth == 2 || depth == 3) {
+            continue;
+        }
+        let y = depths.slice(depth-3, depth);
+        let x = depths.slice(depth-4, depth-1);
+        if (movAvg(y) > movAvg(x)) {
+            i++;
+        }
+
+    }
 }
 console.log(i);
