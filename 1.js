156afa8bb1f8cfa661a2266fdb650090bea586e1
Matt Schlosser <hello@mattschlosser.me>
2021-12-01 04:55:54 +0000

    build: add docker config for dev

--- 1.js
+++ 1.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 let buffer = fs.readFileSync('1');
 let string = buffer.toString();
 let lines = string.split('\n')
+let i = null;
 for (let line of lines) {
     let tokens = line.split(' ');
 
