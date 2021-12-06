b48dc6702f9101f7ef76451c56e8f6937b6eae59
Matt Schlosser <hello@mattschlosser.me>
2021-12-05 22:34:18 -0700

    Simplify day 6

--- 6.js
+++ 6.js
@@ -4,37 +4,26 @@ let string = buffer.toString();
 let lines = string.split('\n');
 let fish = lines[0].split(',')
 
-function range(days) {
-    function *d(days) {
-        for (let i = 0; i < days; i++) {
-            yield i;
-        }
-        yield null;
-    }
-    return d(days);
-}
-// this function takes a list of lines (x1,y1 -> x2,y2), and a function isHit, and returns the number
-// of lines that were hit more than once
+
 let countFish = (fish, days) => {
-    let fishCountA = new Array(9).fill(0)
-    let fishCountB = new Array(9).fill(0)
+    let fishCount = new Array(9).fill(0)
     for (let f of fish) {
-        fishCountA[f] += 1;
+        fishCount[f] += 1;
     }
-    console.log(fishCountA);
-    let gen = range(days);
-    while (gen.next().value !== null) {
-        fishCountB = [...fishCountA];
-        let willReproduce = fishCountB.shift();
-        fishCountB.push(willReproduce);
-        fishCountB[6] += willReproduce;
-        fishCountA = fishCountB;
+    for (let i = 0; i < days; i++) {
+        let willReproduce = fishCount.shift();
+        fishCount.push(willReproduce);
+        fishCount[6] += willReproduce;
     }
-    return fishCountA;
-    // return Object.keys(fishCountA).reduce((a, b) => a + fishCountA[b], 0);
+    return fishCount;
 }
 
+let sumFish = fish => fish.reduce((a, b) => a + b, 0);
+
 // part 1 
-console.log(countFish(fish, 80).reduce((a, b) => a + b, 0));
+console.log(sumFish(countFish(fish, 80)));
+
+// part 2
+console.log(sumFish(countFish(fish, 256)));
 
 
