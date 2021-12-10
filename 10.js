1f681c9d5f2075776078685eb71701ce7ea61cc2
Matt Schlosser <hello@mattschlosser.me>
2021-12-09 22:26:42 -0700

    Day 10: part 2

--- 10.js
+++ 10.js
@@ -10,7 +10,6 @@ let table = {
     '>': 25137,
 }
 let total = 0;
-let stash = [];
 let openingBrackets = ['(', '[', '{', '<'];
 let closingBrackets = [')', ']', '}', '>'];
 let partnerBracket = {
@@ -23,11 +22,22 @@ let partnerBracket = {
     '}': '{',
     '>': '<',
 }
+let autoCompletes = [];
+
+
+let autocmpleteScores = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+}
 
 for (let iine of lines) {
     // determine if line has matching bracket
     let brackets = iine.split('');
     
+    let stash = [];
+    let lineIsCorrupt = false;
     for (let bracket of brackets) {
         if (openingBrackets.includes(bracket)) {
             stash.push(bracket);
@@ -38,14 +48,27 @@ for (let iine of lines) {
                 stash.pop();
             } else {
                 total += table[bracket];
+                lineIsCorrupt = true;
                 break;
             }
         }
     }    
 
-
+    if (!lineIsCorrupt) {
+        let autocompleteScore = 0;
+        // sum up the remianing brackets in the stash according to autoComplete scores
+        stash.reverse()
+        for (let bracket of stash) {
+            autocompleteScore = autocompleteScore * 5 + autocmpleteScores[partnerBracket[bracket]];
+        }
+        autoCompletes.push(autocompleteScore);
+    }
 
 }
 
 //console.log(total);
 console.log(total);
+
+// sort autoCompletes and take the middle value
+autoCompletes.sort((a, b) => a - b);
+console.log(autoCompletes[Math.floor(autoCompletes.length / 2)]);
