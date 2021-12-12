ec844150f6337ad6e8b75512ea70169436e2df40
Matt Schlosser <hello@mattschlosser.me>
2021-12-11 23:57:46 -0700

    I overcame!

--- 12.js
+++ 12.js
@@ -7,7 +7,6 @@ let lines = string.split('\n').filter(e => e != '').map(e => e.split('-').map(e 
 let graph = {};
 
 for (let line of lines) {
-    console.log(line);
     let [from, to] = line;
     if (!graph[from]) {
         graph[from] = [];
@@ -21,44 +20,38 @@ for (let line of lines) {
     if (from != 'start' && to != 'end')
         graph[to].push(from);
 }
-console.log(graph);
-let start = graph.start;
-let end = graph.end;
-let visited = {};
-let queue = ['start'];
-// let visited = {};
-let visits = [];
-// find all the differnet ptahs through the graph
-findAPath = (graph, start, end) => {
-    let visited = {start: true};
-    let visits = [];
-    // depth first search
-    let nodes = graph[start];
-    for (let node of nodes) {
-        if (!visited[node]) {
-            visited[node] = true;
-        }
-        let paths = findAllPaths(graph, node, end);
-    }
-    return visits;
-}
 // console.log(findAPath(graph, 'start', 'end'));
-visit = (node, visited) => {
+visit = (node, visited, part = 1) => {
     let possiblePaths = [];
     if (node == 'end') {
         return [[node]];
     }
     for (let neighbour of graph[node]) {
         if (!visited.includes(neighbour)) {
-            morePossiblePaths = visit(neighbour, [...visited, node]);
+            morePossiblePaths = visit(neighbour, [...visited, node], part);
             for (let path of morePossiblePaths) {
                 possiblePaths.push([node, ...path]);
             }
             // if we are visiting a node that can be visited twice, 
             // do not add it to the visited list
             if (node.toUpperCase() == node) {
-                morePossiblePaths = visit(neighbour, visited);
+                morePossiblePaths = visit(neighbour, visited, part);
                 for (let path of morePossiblePaths) {
+                    possiblePaths.push([node, ...path]);
+                }
+            }
+        } 
+    }
+    if (part == 2) {
+        for (let neighbour of graph[node]) {
+            // console.log("HERE");
+            // all other caves have been visited at most once
+            // we are going to visit a small cave twice
+            if (node.toUpperCase() != node) {
+                let v = visited.filter(e => e != node)
+                morePossiblePaths = visit(neighbour, v, 1);
+                for (let path of morePossiblePaths) {
+                    processedPath = path.filter(e => e == e.toLowerCase());
                     possiblePaths.push([node, ...path]);
                 }
             }
@@ -70,7 +63,12 @@ visit = (node, visited) => {
 // console.log(visit('b', ['d']));
 // console.log(visit('d', ['b']));
 // console.log(visit('A', ['c']));
-console.log(visit('start', []));
+// console.log(visit('start', []));
 let paths = visit('start', []);
 let x = new Set(paths.map(e => e.join(',')))
 console.log(x, x.size);
+let paths2 = visit('start', [], 2);
+let comparer = paths2.map(e => e.filter(e => e.toLowerCase() == e));
+let finalPaths = paths2.filter((e,i) => comparer[i].length - (new Set(comparer[i])).size < 2).map(e => e.join(','))
+let x2 = new Set(finalPaths);
+console.log(x2, x2.size);
