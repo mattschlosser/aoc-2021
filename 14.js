dfead0bcea9b6a817f26dcb7e2ba5deccfcdfbb7
Matt Schlosser <hello@mattschlosser.me>
2021-12-13 23:14:58 -0700

    clean

--- 14.js
+++ 14.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-const { exit } = require('process');
 let buffer = fs.readFileSync('14');
 let string = buffer.toString();
 let [lines, instructions ] = string.split('\n\n');
-// let dots = lines.split('\n').filter(e => e != '');
 instructions = instructions.split('\n').filter(e => e != '');
 
 // find max and min keys of count
@@ -38,18 +36,14 @@ let iterations = (lines, iterations) => {
     }
     // ok so we just count each occurent of each pair initally 
     for (let i = 0; i < iterations; i++) {
-        // break;
         for (let item in c) {   
             let [current, next] = item.split('');
-            // console.log(current, next);
-            // console.log(current, next);
             let value = c[item];
             if (next != undefined) {
                 for (let instruction of instructions) {
                     let [first, second, toInsert] = instruction.split(/ -> |/);
                     if (current == first && next == second) {
                         // add or insert
-                        // d[item] = 0;
                         d[`${first}${toInsert}`] = d[`${first}${toInsert}`] == undefined ? value : d[`${first}${toInsert}`] + value
                         d[`${toInsert}${second}`] = d[`${toInsert}${second}`] == undefined ? value : d[`${toInsert}${second}`] + value
                     }
@@ -62,12 +56,9 @@ let iterations = (lines, iterations) => {
     c[`${lines[lines.length - 2]}_`] = 1;
     let count = {};
     for (let key in c) {
-        // console.log(key, c[key]);
         // split the keys into two parts
         let [first, second] = key.split('');
-        // count each instance of each key
         count[first] = (count[first] || 0) + c[key];
-        // count[second] = (count[second] || 0) + c[key];
     }
     return diff(count);
 }
